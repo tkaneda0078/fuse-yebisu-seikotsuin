2c85567fd751c8f2bc05a937a784e4763b2c8064
金田尚大 <1111510078r21@gmail.com>
2019-05-21 21:58:19 +0900

    card追加

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,10 @@ import React from 'react'
 
 import Layout from '../components/layout'
 import Image from '../components/image'
-
 import SEO from '../components/seo'
+import Card from 'react-bootstrap/Card'
+import CardGroup from 'react-bootstrap/CardGroup'
+import Button from 'react-bootstrap/Button'
 
 const IndexPage = () => (
   <Layout>
@@ -18,6 +20,43 @@ const IndexPage = () => (
         <p>This is a wider card with supporting text below as a natural lead-in to
           additional content. This content is a little bit longer.</p>
       </div>
+    </div>
+    <div>
+      <CardGroup>
+        <Card className='rounded'>
+          <Image className={'card-img rounded'} filename={'sample2.jpg'} alt={''} />
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural lead-in to
+              additional content. This content is a little bit longer.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        <Card className='rounded'>
+          <Image className={'card-img rounded'} filename={'sample2.jpg'} alt={''} />
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural lead-in to
+              additional content. This content is a little bit longer.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        <Card className='rounded'>
+          <Image className={'card-img rounded'} filename={'sample2.jpg'} alt={''} />
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural lead-in to
+              additional content. This content is a little bit longer.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </CardGroup>
     </div>
   </Layout>
 )
