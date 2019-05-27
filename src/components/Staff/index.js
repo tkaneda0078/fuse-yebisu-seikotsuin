71086dbc935d209a98c9eb5545a833db22f7c1f0
金田尚大 <1111510078r21@gmail.com>
2019-05-27 18:09:35 +0900

    スタッフ一覧修正

--- src/components/Staff/index.js
+++ src/components/Staff/index.js
@@ -1,24 +1,59 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Image from '../image'
+import Figure from 'react-bootstrap/Figure'
 
-const Staff = () => (
-  <Container className='staff-contents' fluid>
-    <Row>
-      <Col>
-        <Image className={'staff-img img-circle'} filename={'sample2.jpg'} alt={''} />
-      </Col>
-      <Col>
-        <Image className={'staff-img'} filename={'sample2.jpg'} alt={''} />
-      </Col>
-      <Col>
-        <Image className={'staff-img'} filename={'sample2.jpg'} alt={''} />
-      </Col>
-    </Row>
-  </Container>
-)
+class Staff extends Component {
+  constructor() {
+    super();
+    this.state = {
+      profiles: [
+        {
+          name: '金田　俊行',
+          position: '院長',
+          image: 'sample2.jpg'
+        },
+        {
+          name: '池田　大典',
+          position: '仕事人',
+          image: 'sample2.jpg'
+        },
+        {
+          name: '石神　良太',
+          position: '柔道整復師・鍼灸師',
+          image: 'sample2.jpg'
+        }
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <Container className='staff-contents'>
+        <Row>
+          {
+            this.state.profiles.map(profile => (
+              <Col className='text-center'>
+                <Figure>
+                  <Image className={'staff-img img-circle'} filename={profile.image} alt={''} />
+                  <Figure.Caption className='text-center mt-3'>
+                    <h4>
+                      <p className='mb-2'>{profile.position}</p>
+                      <p>{profile.name}</p>
+                    </h4>
+                  </Figure.Caption>
+                </Figure>
+              </Col>
+            ))
+          }
+        </Row>
+      </Container>
+    )
+  }
+
+}
 
 export default Staff
