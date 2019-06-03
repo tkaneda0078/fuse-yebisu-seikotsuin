9913928aa2f8ad8c7ae6a8ed4ca76691cce451d7
金田尚大 <1111510078r21@gmail.com>
2019-06-03 15:01:35 +0900

    各リンク名を変更

--- src/components/GlobalNav/index.js
+++ src/components/GlobalNav/index.js
@@ -4,21 +4,24 @@ import Nav from 'react-bootstrap/Nav'
 
 // TODO: class化
 const GlobalNav = () => (
-  <Nav justify defaultActiveKey="/home" as="ul">
-    <Nav.Item as="li">
-      <Nav.Link href="/home">当院について</Nav.Link>
+  <Nav justify as='ul'>
+    <Nav.Item as='li'>
+      <Nav.Link href='/about'>当院について</Nav.Link>
     </Nav.Item>
-    <Nav.Item as="li">
-      <Nav.Link eventKey="link-2">ポリシー</Nav.Link>
+    <Nav.Item as='li'>
+      <Nav.Link href='/service'>施術内容</Nav.Link>
     </Nav.Item>
-    <Nav.Item as="li">
-      <Nav.Link eventKey="link-2">施術内容</Nav.Link>
+    <Nav.Item as='li'>
+      <Nav.Link href='/staff'>スタッフ</Nav.Link>
     </Nav.Item>
-    <Nav.Item as="li">
-      <Nav.Link eventKey="link-1">スタッフ紹介</Nav.Link>
+    <Nav.Item as='li'>
+      <Nav.Link href='/news'>お知らせ</Nav.Link>
     </Nav.Item>
-    <Nav.Item as="li">
-      <Nav.Link eventKey="link-2">お知らせ</Nav.Link>
+    <Nav.Item as='li'>
+      <Nav.Link href='/question'>よくある質問</Nav.Link>
+    </Nav.Item>
+    <Nav.Item as='li'>
+      <Nav.Link href='/access'>アクセス</Nav.Link>
     </Nav.Item>
   </Nav>
 )
