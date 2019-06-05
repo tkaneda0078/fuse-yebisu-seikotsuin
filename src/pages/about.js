78d3db662d81fd46f0622aecfbcf5a12e290f883
金田尚大 <1111510078r21@gmail.com>
2019-06-05 15:44:15 +0900

    コンセプト追加

--- src/pages/about.js
+++ src/pages/about.js
@@ -18,6 +18,7 @@ const IndexPage = () => (
 
       <div className='about-contents'>
         <h1>老若男女問わず、患者様一人ひとりのお気持ちに寄り添います</h1>
+
         <div className='policy-content'>
           <figure>
             <Image className={'policy-img'} filename={'sample2.jpg'} alt={''} />
@@ -25,10 +26,39 @@ const IndexPage = () => (
           <div className='policy-caption'>
             <p>業界歴10年のスタッフが、患者様一人ひとりのお悩みにお応えします</p>
             <p>歪んだ体・お顔に徹底アプローチ！</p>
-            <p>長年の痛みや不調もお任せください◎</p>
+            <p>長年の痛みや不調もお任せください</p>
+            <p>当院では、小顔リフト矯正をはじめ、身体の土台や軸となる部分を矯正する施術も行っております。</p>
+            <p>身体の軸や土台となる箇所の矯正施術</p>
+            <p>歪んだ体・お顔に徹底アプローチ！</p>
+            <p>長年の痛みや不調もお任せください</p>
             <p>当院では、小顔リフト矯正をはじめ、身体の土台や軸となる部分を矯正する施術も行っております。</p>
           </div>
         </div>
+
+        <div className='concept-contents'>
+          <div className='concept'>
+            <figure>
+              <Image className={'img'} filename={'sample2.jpg'} alt={''} />
+            </figure>
+            <div className='caption'>
+                <h3>布施で一番【笑顔】が多い整骨院を目指します！</h3>
+                <p>この業界に勤めて約10年。有難いことに、これまでたくさんの方々に支えられ「布施　ゑびす整骨院」を開院することができました。</p>
+                <p>地域の皆様が元気な体で、笑顔あふれる生活をお送りできますよう、精一杯サポートさせていただきます。</p>
+                <p>些細なことでも構いません。体に気になることがございましたら、何なりとご相談ください。</p>
+            </div>
+          </div>
+          <div className='concept'>
+            <figure>
+              <Image className={'img'} filename={'sample2.jpg'} alt={''} />
+            </figure>
+            <div className='caption'>
+                <h3>ホッと一息つける温かみのある院内です</h3>
+                <p>院内は、白と緑を基調とした清潔感あふれる空間となっています。また、開放感のある広々とした院内ですので、初めてご来院される方も女性の方もリラックスしてお過ごしいただけます。</p>
+                <p>心も体も落ち着ける環境を整えていますので、気を楽にしてお越しください。</p>
+            </div>
+          </div>
+        </div>
+
       </div>
 
     </Container>
