44dd48987c7e335a13b0cd893df54e98a1b131af
金田尚大 <1111510078r21@gmail.com>
2019-06-26 15:18:01 +0900

    ナブバー調整

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,61 +1,10 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-
+import React  from 'react'
 import Navbar from '../Navbar'
 
-class Header extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      siteTitle: props.siteTitle,
-      currentPosition: 0,
-      headerClass: 'header hidden'
-    }
-  }
-
-  componentDidMount() {
-    window.addEventListener('scroll', event => this.watchCurrentPosition(), true)
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('scroll', {})
-  }
-
-  scrollTop() {
-    return Math.max(
-      window.pageYOffset,
-      document.documentElement.scrollTop,
-      document.body.scrollTop);
-  }
-
-  /**
-   * 現在位置を見る
-   * スクロール位置が10より大きい場合header背景色を変更する
-   */
-  watchCurrentPosition() {
-    if (this.scrollTop() > 10) {
-      this.setState({ headerClass: 'header' })
-    } else {
-      this.setState({ headerClass: 'header hidden' })
-    }
-  }
-
-  render() {
-    return (
-      <header className={this.state.headerClass}>
-        <Navbar />
-      </header>
-    )
-  }
-
-}
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
+const Header = () => (
+  <header className='header'>
+    <Navbar />
+  </header>
+)
 
 export default Header
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,31 +3,29 @@ import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 
 const GlobalNavbar = (props) => (
-  <Navbar bg='light' expand='lg'>
-    <Navbar.Brand href='/'>{props.siteTitle}</Navbar.Brand>
+  <Navbar expand='lg'>
+    <Navbar.Brand href='/' className='ml-md-5'>{props.siteTitle}</Navbar.Brand>
     <Navbar.Toggle aria-controls='navbar_responsive' />
     <Navbar.Collapse id='navbar_responsive'>
-      <Nav className='ml-auto'>
-        <ul className='navbar-nav ml-auto my-2 pr-2'>
-          <Nav.Item className='nav-item' as='li'>
-            <Nav.Link href='/about'>当院について</Nav.Link>
-          </Nav.Item>
-          <Nav.Item className='nav-item' as='li'>
-            <Nav.Link href='/first-time'>初めての方へ</Nav.Link>
-          </Nav.Item>
-          <Nav.Item className='nav-item' as='li'>
-            <Nav.Link href='/service'>施術内容</Nav.Link>
-          </Nav.Item>
-          <Nav.Item className='nav-item' as='li'>
-            <Nav.Link href='/staff'>スタッフ</Nav.Link>
-          </Nav.Item>
-          <Nav.Item className='nav-item' as='li'>
-            <Nav.Link href='/faq'>よくある質問</Nav.Link>
-          </Nav.Item>
-          <Nav.Item className='nav-item' as='li'>
-            <Nav.Link href='/access'>アクセス</Nav.Link>
-          </Nav.Item>
-        </ul>
+      <Nav className='nav my-2 pr-3 text-right ml-auto' as='ul'>
+        <Nav.Item className='li' as='li'>
+          <Nav.Link href='/about'>当院について</Nav.Link>
+        </Nav.Item>
+        <Nav.Item className='li' as='li'>
+          <Nav.Link href='/first-time'>初めての方へ</Nav.Link>
+        </Nav.Item>
+        <Nav.Item className='li' as='li'>
+          <Nav.Link href='/service'>施術内容</Nav.Link>
+        </Nav.Item>
+        <Nav.Item className='li' as='li'>
+          <Nav.Link href='/staff'>スタッフ</Nav.Link>
+        </Nav.Item>
+        <Nav.Item className='li' as='li'>
+          <Nav.Link href='/faq'>よくある質問</Nav.Link>
+        </Nav.Item>
+        <Nav.Item className='li' as='li'>
+          <Nav.Link href='/access'>アクセス</Nav.Link>
+        </Nav.Item>
       </Nav>
     </Navbar.Collapse>
   </Navbar>
