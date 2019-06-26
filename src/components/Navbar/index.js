fadae698a32ffab8ab032e8b9b47b6f98d916444
金田尚大 <1111510078r21@gmail.com>
2019-06-26 12:25:59 +0900

    ナビトリガー修正

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,37 +1,36 @@
 import React from 'react'
 import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
 
-const Navbar = (props) => (
-  <Nav className='nav navbar navbar-expand-lg navbar-light fixed-top py-3'>
-    <div className='container'>
-      <a className='navbar-brand js-scroll-trigger' href='/'>{props.siteTitle}</a>
-      <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarResponsive' aria-controls='navbarResponsive' aria-expanded='false' aria-label='Toggle navigation'>
-        <span className='navbar-toggler-icon'></span>
-      </button>
-      <div className='collapse navbar-collapse' id='navbarResponsive'>
-        <ul className='navbar-nav ml-auto my-2 my-lg-0'>
+const GlobalNavbar = (props) => (
+  <Navbar bg='light' expand='lg'>
+    <Navbar.Brand href='/'>{props.siteTitle}</Navbar.Brand>
+    <Navbar.Toggle aria-controls='navbar_responsive' />
+    <Navbar.Collapse id='navbar_responsive'>
+      <Nav className='ml-auto'>
+        <ul className='navbar-nav ml-auto my-2 pr-2'>
           <Nav.Item className='nav-item' as='li'>
-            <Nav.Link href='/about' className='js-scroll-trigger'>当院について</Nav.Link>
+            <Nav.Link href='/about'>当院について</Nav.Link>
           </Nav.Item>
           <Nav.Item className='nav-item' as='li'>
-            <Nav.Link href='/first-time' className='js-scroll-trigger'>初めての方へ</Nav.Link>
+            <Nav.Link href='/first-time'>初めての方へ</Nav.Link>
           </Nav.Item>
           <Nav.Item className='nav-item' as='li'>
-            <Nav.Link href='/service' className='js-scroll-trigger'>施術内容</Nav.Link>
+            <Nav.Link href='/service'>施術内容</Nav.Link>
           </Nav.Item>
           <Nav.Item className='nav-item' as='li'>
-            <Nav.Link href='/staff' className='js-scroll-trigger'>スタッフ</Nav.Link>
+            <Nav.Link href='/staff'>スタッフ</Nav.Link>
           </Nav.Item>
           <Nav.Item className='nav-item' as='li'>
-            <Nav.Link href='/faq' className='js-scroll-trigger'>よくある質問</Nav.Link>
+            <Nav.Link href='/faq'>よくある質問</Nav.Link>
           </Nav.Item>
           <Nav.Item className='nav-item' as='li'>
-            <Nav.Link href='/access' className='js-scroll-trigger'>アクセス</Nav.Link>
+            <Nav.Link href='/access'>アクセス</Nav.Link>
           </Nav.Item>
         </ul>
-      </div>
-    </div>
-  </Nav>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
 )
 
-export default Navbar
+export default GlobalNavbar
