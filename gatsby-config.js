fa2fea458bab67bb7fa195524aeff89e659508b0
金田尚大 <1111510078r21@gmail.com>
2019-07-03 15:08:07 +0900

    ビルド

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,5 @@
-let activeEnv =
-  process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
-
-console.log(`Using environment config: '${activeEnv}'`)
-
 require("dotenv").config({
-  path: `.env.${activeEnv}`,
+  path: `.env.${process.env.NODE_ENV}`,
 })
 
 
