cf8876e8d6c49a9c675b3ffd030ce7a3868f7ed0
金田尚大 <1111510078r21@gmail.com>
2019-07-17 21:55:15 +0900

    env書き方変更

--- src/components/GoogleMap/index.js
+++ src/components/GoogleMap/index.js
@@ -22,7 +22,6 @@ class GoogleMap extends Component {
     return (
       <div className='google-map'>
         <GoogleMapReact
-          // bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
           bootstrapURLKeys={{ key: process.env.GATSBY_GOOGLE_API_KEY }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
