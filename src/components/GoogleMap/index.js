833ac38ea89e5447d2e9c3e82349028e3903e1bb
金田尚大 <1111510078r21@gmail.com>
2019-07-17 21:18:09 +0900

    ビルド

--- src/components/GoogleMap/index.js
+++ src/components/GoogleMap/index.js
@@ -22,7 +22,8 @@ class GoogleMap extends Component {
     return (
       <div className='google-map'>
         <GoogleMapReact
-          bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
+          // bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
+          bootstrapURLKeys={{ key: process.env.GOOGLE_MAP_API_KEY }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
           onGoogleApiLoaded={({ map, maps }) => this.renderMarkers(map, maps)}
